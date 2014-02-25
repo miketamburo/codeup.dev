b41a03004db150bb7de46bf0b64ad620415b07cb
miketamburo <matsatam@gmail.com>
2014-02-25 11:38:04 -0600

    Todo List Uploading a file

--- public/todo-list.php
+++ public/todo-list.php
@@ -25,7 +25,27 @@ function saveFile($filename, $items){
 
 $filename = "data/todoitems.txt";
 $items = loadFile($filename);
+//File to upload script
+// Verify there were uploaded files and no errors
+if (count($_FILES) > 0 && $_FILES['fileUpLoad']['error'] == 0) {
+    // Set the destination directory for uploads
+    $upload_dir = '/vagrant/sites/codeup.dev/public/uploads/';
+    // Grab the filename from the uploaded file by using basename
+    $uploadfilename = basename($_FILES['fileUpLoad']['name']);
+    // Create the saved filename using the file's original name and our upload directory
+    $saved_filename = $upload_dir . $uploadfilename;
+    // Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['fileUpLoad']['tmp_name'], $saved_filename);
+}
 
+// Check if we saved a file
+if (isset($saved_filename)) {
+    // If we did, show a link to the uploaded file
+    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+}
+
+
+// Add an item to the list
 if (isset($_POST['enter_item']) && !empty($_POST['enter_item'])){
     $item = ucfirst($_POST['enter_item']);
     array_push($items, $item);
@@ -34,6 +54,7 @@ if (isset($_POST['enter_item']) && !empty($_POST['enter_item'])){
 	saveFile($filename, $items);    
 }
 
+// Remove an item from the list
 if (isset($_GET['remove'])) {
 	$key = $_GET['remove'];
 	unset($items[$key]);
@@ -65,18 +86,18 @@ if (isset($_GET['remove'])) {
 
 			<p>Enter your item or choose your option.</p>	
 
-			<form method="POST" action="">
+			<form method="POST" enctype="multipart/form-data" action="">
 				<p> 
 					<p>
 						<label for='enter_item'> Item to add: </label>
 	        			<input id="enter_item" name="enter_item" type="text" autofocus = 'autofocus' placeholder="Enter new item" style="width:200px;">
+				      
+				        <input type="submit" value="Add item" />
 				    </p>
 				    <p>
 						<label for='fileUpLoad'> File to upload: </label>
 	        			<input id='fileUpLoad' name='fileUpLoad' type="file">
-				    </p>
-				    <p>    
-				        <input type="submit" value="Add item" />
+	        			<input type="submit" value="Upload File" />
 				    </p>
 				</p>
 			</form>
