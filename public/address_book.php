d0242d833d408a48ba9ba2b4ce9ecae9a98e9f94
miketamburo <matsatam@gmail.com>
2014-02-26 11:05:00 -0600

    Address Book
    Created shell and added title and 6 entry blocks

---
+++ public/address_book.php
@@ -0,0 +1,38 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Address Book</title>
+</head>
+<body>
+	<h2>Address Book</h2>
+
+	<form method="POST" action="">
+		<label for='personName'> Name: </label>
+	    <input id="personName" name="personName" type="text" autofocus = 'autofocus' tab=1 placeholder="Enter First and Last Name" style="width:200px;">
+	    <p></p>
+	    <label for='address'> Address: </label>
+	    <input id='address' name='address' type="text" tab=2 placeholder="Enter First and Last Name" style="width:200px;">
+	    <p></p>
+	    <label for='city'> City: </label>
+	    <input id="city" name="city" type="text" tab=3 placeholder="Enter First and Last Name" style="width:200px;">
+	    <p></p>
+	    <label for='state'> State: </label>
+	    <input id="state" name="state" type="text" tab=4 placeholder="Enter First and Last Name" style="width:200px;">
+	    <p></p>
+	    <label for='zip'> Zip: </label>
+	    <input id="zip" name="zip" type="text" tab=5 placeholder="Enter First and Last Name" style="width:200px;">
+	    <p></p>
+	    <label for='phone'> Phone: </label>
+	    <input id="phone" name="phone" type="text" tab=6 placeholder="Enter First and Last Name" style="width:200px;">
+
+	</form>
+
+
+</body>
+</html>
