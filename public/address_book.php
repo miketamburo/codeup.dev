f23cca082b5cb7bbc79c24382657bcf9da971d1b
miketamburo <matsatam@gmail.com>
2014-02-28 09:53:10 -0600

    Address Book using include

--- public/address_book.php
+++ public/address_book.php
@@ -17,32 +17,8 @@ $zip = '';
 $phone = '';
 $fileUploadError = '';
 // establish a class
-class AddressDataStore {
-	public $filename = '';
+include_once('Classes/address_data_store.php');
 
-	function read_address_book() {
-		$contents = [];
-		$handle = fopen($this->filename, 'r');
-		while(($data = fgetcsv($handle)) !== FALSE) {
-	  	$contents[] = $data;
-		}
-		fclose($handle);
-		return $contents;
-	}
-
-	function write_address_book($addresses_array) {
-		$handle = fopen($this->filename, 'w');
-			foreach ($addresses_array as $fields) {
-				fputcsv($handle, $fields);
-			}
-		fclose($handle);
-	}
-
-	function __construct($filename = "data/address_book.csv"){
-		$this->filename = $filename;
-	}
-	
-}
 $book = new AddressDataStore();
 
 $addresses_array = $book->read_address_book();
