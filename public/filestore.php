2f9d535e05d12f3c1a5c80f0fa39768d2c44c528
miketamburo <matsatam@gmail.com>
2014-03-03 10:26:19 -0600

    filestore.php
    Updated version determines which method to use based on if file is a
    txt or csv file type.

--- public/filestore.php
+++ public/filestore.php
@@ -4,13 +4,36 @@ class Filestore {
 	public $filename = '';
 	public $items = [];
 	public $contents = [];
+	private $is_csv = FALSE;
 
 	public function __construct($filename = ''){
 		$this->filename = $filename;
+		if (substr($filename, -3)) == 'csv'){
+			$this->$is_csv = TRUE;
+		} elseif (substr($filename, -3)) == 'txt'){
+			$this->$is_csv = FALSE;
+		} // consider later addition to address what happens if other file types
+		// are introduced.
+	}
+
+	public function read(){
+		if ($this->$is_csv == TRUE) {
+			$this->read_csv();
+		} elseif ($this->$is_csv == FALSE){
+			$this->read_lines();
+		}
+	}
+
+	public function write($array){
+		if ($this->$is_csv == TRUE) {
+			$this->write_csv();
+		} elseif ($this->$is_csv == FALSE){
+			$this->write_lines();
+		}
 	}
 
 	// returns array of lines in $this->filename
-	public function read_lines(){
+	private function read_lines(){
 		$handle = fopen($this->filename, "r");
 		$fileSize1 = filesize($this->filename);
 		if ($fileSize1 > 0){
@@ -27,7 +50,7 @@ class Filestore {
 		return $contents;
 	}
 	// writes each element in $array to a new line in $this-> filename
-	public function write_lines($items){
+	private function write_lines($items){
 		$handle = fopen($this->filename, "w");
         $item = implode("\n", $items);
         fwrite($handle, $item);
@@ -36,7 +59,7 @@ class Filestore {
     }
 
 	// Reads contents of csv $this->filename, returns an array
-	public function read_csv(){
+	private function read_csv(){
 		$contents = [];
 		$handle = fopen($this->filename, 'r');
 		while(($data = fgetcsv($handle)) !== FALSE) {
@@ -46,7 +69,7 @@ class Filestore {
 		return $contents;
 	}
 
-	public function write_csv($contents){
+	private function write_csv($contents){
 		$handle = fopen($this->filename, 'w');
 			foreach ($contents as $fields) {
 				fputcsv($handle, $fields);
