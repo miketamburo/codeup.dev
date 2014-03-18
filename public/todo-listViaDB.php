703e2c6ca1023cbadf9baed071753d3c31611b44
miketamburo <matsatam@gmail.com>
2014-03-18 13:29:12 -0500

    Todo List using mysql - order list, pager offset, insert, delete items

--- public/todo-listViaDB.php
+++ public/todo-listViaDB.php
@@ -1,168 +1,178 @@
 <?php
-// The To Do List Project using mysql 
-$errorMessage = "";
-// ====================================================================================
 // Get new instance of MySQLi object
 $mysqli = @new mysqli('127.0.0.1', 'mike', 'password', 'todo_db');
+// set initial variable values
+$errorMsg = ' ';
+$limit = 10;
 // Check for errors
 if ($mysqli->connect_errno) {
     echo 'Failed to connect to MySQL: (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error;
 } else {
   echo $mysqli->host_info . "\n";
 }
+// Query for total rows ==============================================================
+$totalRows = $mysqli->query('SELECT * FROM todo_db');
 
-// sort the list 
-if (isset($_GET['sort_col']) && isset($_GET['sort_order'])){
-  $sortCol = $_GET['sort_col'];
-  $sortOrd = $_GET['sort_order'];  
-  $result = $mysqli->query("SELECT * FROM todo_db ORDER BY $sortCol $sortOrd"); 
+$num_rows = $totalRows->num_rows;
+
+$num_pages = ceil($num_rows/$limit);
+
+$totalRows->close();
+
+// Calculate the number of pages required and calculate offset ==========================
+if (!empty($_GET['page'])){
+  $page = $_GET['page'];
 } else {
-  $result = $mysqli->query("SELECT * FROM todo_db"); 
+  $page = 1;
 }
 
-// create prepard statements ===============================================
-if ((isset($_POST['thing_to_do'])) && (!empty($_POST['thing_to_do'])))
+if ($page > 1){
+  $offset = (($_GET['page'] * $limit) - $limit);
+} else {
+  $offset = 0;
+}
+
+// Create prepared statement for data pull =============================================
+$stmt = $mysqli->prepare("SELECT * FROM todo_db LIMIT ? OFFSET ?");
+
+// bind params
+$stmt->bind_param("ii", $limit, $offset);
+
+// execute the query and return the result
+$stmt->execute();
+
+// bind the result
+$stmt->bind_result($id, $thing_to_do, $completed);
+
+$rows = array();
+
+while ($stmt->fetch()){
+  $rows[] = array('id' => $id, 'thing_to_do' => $thing_to_do, 'completed' => $completed);
+}
+
+// create prepared statements for INSERT ===============================================
+if (!empty($_POST['thing_to_do']))
 {
-// prepare the statement - use ? as placeholder =====================================================
-	$stmt = $mysqli->prepare("INSERT INTO todo_db (thing_to_do) VALUES (thing_to_do = ?)");
-
-// bind the parameters of the statement
-	$stmt->bind_param("ssssd", $_POST['thing_to_do']);       
-
-// execute the insert
-	$stmt->execute();
-
+  // prepare the statement - use ? as placeholder ======================================
+  $stmt = $mysqli->prepare("INSERT INTO todo_db (thing_to_do, completed) VALUES (?, ?)");
+  // bind the parameters of the statement
+  $stmt->bind_param("sd", (htmlspecialchars(strip_tags($_POST['thing_to_do']))), $_POST['completed']);       
+  // execute the insert
+  $stmt->execute();
 } elseif ((empty($_POST))) {
   $errorMsg = ' ';
-} elseif ((isset($_POST)) && (empty($_POST['thing_to_do']))){
+} elseif (isset($_POST) && empty($_POST['thing_to_do']))
+{
   $errorMsg = "Error:  Unable to update the list due to an empty field submission.";
-}        
+} 
 
-try {
-// Add an item to the list
-	if (isset($_POST['thing_to_do'])){
-
-	    $checkValue = $_POST['thing_to_do'];
-	    if ((strlen($checkValue) < 240) && !empty($_POST['thing_to_do'])){
-	    	$item = ucfirst(htmlspecialchars(strip_tags($checkValue)));
-	    	array_push($items, $item);
-		
-// INSERT ITEMS TO DATABASE //////
-
-	    } else {
-	    	throw new InvalidInputException ('No item entered or value is longer than 240 characters');
-	    }   	   
-	}
-} catch (InvalidInputException $e) {
-	if (empty($_POST['thing_to_do'])){
-		echo $e->getMessage() . PHP_EOL;
-	}	
+// MARK AN ITEM AS COMPLETE AND REMOVE IT
+if (isset($_GET['remove'])){
+  // prepare the statement - use ? as placeholder ======================================
+  $stmt = $mysqli->prepare("DELETE FROM todo_db WHERE id = ?");
+  // bind the parameters of the statement
+  $stmt->bind_param("i", $_GET['remove']);       
+  // execute the insert
+  $stmt->execute();
 }
 
-// Remove an item from the list
-if (isset($_GET['remove'])) {
-	$key = $_GET['remove'];
-// Save completed items to an archive file (AT THE END OF THE FILE)
-	$archiveArray = $class->read($archiveFile);
-	array_push($archiveArray, $items[$key]);
-		
-// Remove item from list and save new todo list
+///// BEGIN VALIDATION CODE 
+$validCols = array('thing_to_do', 'completed');
+// set default values
+$sortCol = 'thing_to_do';
+$sortOrd = 'ASC';
 
-	header("Location: todo-listViaDB.php");	
+if (isset($_GET['offset']) && $_GET['offset'] == 10){
+  $offset = 10;
 }
-///////////////////////////////////////////////////////////////////////////
+if (isset($_GET['sort_col']) && in_array($_GET['sort_col'], $validCols))
+{
+  $sortCol = $_GET['sort_col'];
+}
+if (isset($_GET['sort_order']) && $_GET['sort_order'] == 'DESC')
+{
+  $sortOrd = 'DESC';
+}
+
+$result = $mysqli->query("SELECT * FROM todo_db ORDER BY $sortCol $sortOrd LIMIT 10 OFFSET $offset");
+///// END VALIDATION CODE
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+	  <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Pal it">
     <meta name="author" content="Michael Tamburo">
     
-	<title>To Do List</title>
+	  <title>To Do List</title>
 
-  <link rel="shortcut icon" href="/css/bootstrap-3.1.1/docs/assets/ico/favicon.ico">
-	<link href="/css/bootstrap-3.1.1/css/bootstrap.min.css" rel="stylesheet" >
-	<link href="/css/bootstrap-3.1.1/docs/examples/carousel/carousel.css" rel="stylesheet">
-	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-	<link href="/css/national_parks.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/css/bootstrap-3.1.1/docs/assets/ico/favicon.ico">
+	  <link href="/css/bootstrap-3.1.1/css/bootstrap.min.css" rel="stylesheet" >
+	  <link href="/css/bootstrap-3.1.1/docs/examples/carousel/carousel.css" rel="stylesheet">
+	  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	  <link href="/css/todo.css" rel="stylesheet">
 </head>
 <!-- NAVBAR ============================================================================ -->
 <body>
-    <div class="navbar-wrapper">      
-      <div class="container">
-        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">          
-          <div class="container">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              </button><h3 class="whiteletters" class="navbar-brand">To Do List</h3><h4><?=$errorMsg ?></h4>
-            </div>
-            <!-- End of navbar header ===========-->
-            </div>
-            <!-- End of navbar collapse info ===========-->
-          </div><!-- /. container ========-->
-        </div>
-        <!-- End of Navbar features ================-->
-      </div>
-    </div><!-- End of Navbar Wrapper =================-->
+    <div class="navbar-wrapper">
+        <div class="container">
+            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <h3 class="whiteletters" class="navbar-brand">To Do List</h3>
+                        <h4 class="whiteletters"><?=$errorMsg ?></h4>
+                    </div><!-- End of navbar header ===========-->
+                </div><!-- End of navbar collapse info ===========-->
+            </div><!-- /. container ========-->
+        </div><!-- End of Navbar features ================-->
+    </div>
 <!-- ================================================== -->
-<div class="padding">
-  <div>
-  	//////////////////////
-<ul>
-<? foreach($todoList as $key => $item): ?>
-	<li><?= $item; ?> <button onclick="removeById(<?= $key; ?>)">Remove</button></li>
-<? endforeach; ?>
-</ul>
- 
-<form id="removeForm" action="todo-db.php" method="post">
-	<input id="removeId" type="hidden" name="remove" value="">
-</form>
- 
-<script>
-	
-	var form = document.getElementById('removeForm');
-	var removeId = document.getElementById('removeId');
- 
-	function removeById(id) {
-		removeId.value = id;
-		form.submit();
-	}
-</script>
-/////////////////////////////////////////////////
-		<h2 class=center></h2>
-			<p class=center>Enter your item and choose your option.</p>
-				<form method="POST" action="">
-					<label for='thing_to_do'> Item to add: </label>
-        			<input id="thing_to_do" name="thing_to_do" type="text" autofocus = 'autofocus' placeholder="Enter new item" style="width:200px;">
-			        <input type="submit" value="Add item" />
-				</form>
-			<? if (!empty($errorMessage)):?> <?=$errorMessage; endif; ?>
-  	////////////////////////////////////////////////
-  </div>  
-  <table class="table">
-      <tr>
-          <th scope="col">Item sort
-              <a href="?sort_col=thing_to_do&sort_order=ASC"><i class="fa fa-chevron-up"></i></a>
-              <a href="?sort_col=thing_to_do&sort_order=DESC"><i class="fa fa-chevron-down"></i></a>
-          </th>
-      </tr>
-      <? while ($row = $result->fetch_array(MYSQLI_ASSOC)): ?>
-         <tr> 
-          <td><?=$row['thing_to_do']; ?></td>
-        </tr>
-      <? endwhile; ?>
-  </table>
-</div>
+    <div class="container" style="margin-top: 36px;">
+      <form method="POST" action="todo-listViaDB.php"> 
+        <table class="table">
+            <tr>
+                <th scope="col">Item sort
+                    <a href="?sort_col=thing_to_do&amp;sort_order=ASC"><i class="fa fa-chevron-up"></i></a>
+                    <a href="?sort_col=thing_to_do&amp;sort_order=DESC"><i class="fa fa-chevron-down"></i></a>
+                </th>
+            </tr>
 
-<!-- ============================================================================================ -->
+            <? while ($row = $result->fetch_array(MYSQLI_ASSOC)): ?>
+                <tr>
+                    <td><?=$row['thing_to_do']; ?></td>
+                    <td><a href="?remove=<?=$row['id']?>">Mark Complete</a></td> 
+                </tr>
+            <? endwhile; ?>
+
+        </table>
+        
+            <label for='thing_to_do'><h3> Item to add: </h3></label>
+            <input id="thing_to_do" name="thing_to_do" type="text" autofocus = 'autofocus' placeholder="Enter new item" style="width:200px;">
+            <input type="submit" value="Add item" />
+    </form>
+
+        <!-- pager buttons ============================== -->
+        <div>
+          <ul class="pager">
+            <li><a href="?offset=<?=$offset?>">Previous</a></li>
+            <li><a href="?offset=<?=$offset + $limit?>">Next 10</a></li>
+          </ul>
+        </div>
+
+
+        <!-- ============================================ -->
+    </div>
+
+<!-- ================================================== -->
 <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
           
     <div class="container">
 
-      <!-- FOOTER ==============================================================================-->
+      <!-- FOOTER ==================================-->
       <footer>
         <p class="whiteletters">&copy; 2014 Michael Tamburo All rights reserved.</p>
       </footer>
@@ -170,14 +180,10 @@ if (isset($_GET['remove'])) {
     </div><!-- /.container -->
 
 </div>
-    <!-- Bootstrap core JavaScript ===================================================================== -->
+    <!-- Bootstrap core JavaScript ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="css/bootstrap-3.1.1/js/bootstrap.min.js"></script>
     <script src="css/bootstrap-3.1.1/docs/assets/js/docs.min.js"></script>
   </body>
 </html>
-
-
-
-
