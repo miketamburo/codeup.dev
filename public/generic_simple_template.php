5aa4269eed6c68b26b2802de3f66512cb1162652
miketamburo <matsatam@gmail.com>
2014-03-19 11:21:32 -0500

    generic template for quick use

---
+++ public/generic_simple_template.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="address book">
+    <meta name="author" content="Michael Tamburo">
+    
+	<title>Generic</title>
+
+    <link rel="shortcut icon" href="/css/bootstrap-3.1.1/docs/assets/ico/favicon.ico">
+	<link href="/css/bootstrap-3.1.1/css/bootstrap.min.css" rel="stylesheet" >
+	<link href="/css/bootstrap-3.1.1/docs/examples/carousel/carousel.css" rel="stylesheet">
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	
+</head>
+<!-- NAVBAR ============================================================================ -->
+<body>
+    <div class="navbar-wrapper">  
+      <div class="container">
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation"> 
+          <div class="container">
+            <div class="navbar-header">
+              </button><h3><em><a class="whiteletters" class="navbar-brand" href="#">Generic</a></em></h3>
+            </div>
+          </div><!-- End of navbar collapse info ===========-->
+        </div><!-- /. container ========-->
+      </div><!-- End of Navbar features ================-->
+    </div><!-- End of Navbar Wrapper =================-->
+
+<!-- ================================================== -->
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+        <div class="container">
+        <!-- FOOTER ==================================-->
+        <footer>
+            <p class="whiteletters">&copy; 2014 Michael Tamburo All rights reserved.</p>
+        </footer>
+
+        </div><!-- /.container -->
+    </div>
+
+<!-- Bootstrap core JavaScript Placed At End To Assist In Page Loading  ========================== -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="css/bootstrap-3.1.1/js/bootstrap.min.js"></script>
+    <script src="css/bootstrap-3.1.1/docs/assets/js/docs.min.js"></script>
+  </body>
+</html>
